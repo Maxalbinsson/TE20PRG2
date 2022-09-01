d1dfea796081ff2b862a39354e4a60f9a81efda8
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-09-01 13:34:51 +0200

    Kanske gick bra?

---
+++ src/Kryptering.java
@@ -0,0 +1,16 @@
+public class Kryptering {
+    public static void main(String[]args){
+
+        int a = 123;
+        int b = 321;
+
+
+        System.out.print(Integer.toBinaryString(a));
+        System.out.print(Integer.toBinaryString(b));
+
+        System.out.print(a^b);
+
+
+    }
+
+}
