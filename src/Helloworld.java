066d7e4d4b0607e2fa08401325321ff381bcc33b
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-09-02 13:33:00 +0200

    Kanske gick bra?

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,6 +1,12 @@
 public class Helloworld{
     public static void main(String[]args) {
 
+        int a = 123;
+        int b = 321;
+
+        System.out.print(Integer.toBinaryString(b));
+        System.out.print(Integer.toBinaryString(a));
+        System.out.print(a^b);
     }
     ;
 
--- src/Kryptering.java
+++ src/Kryptering.java
@@ -3,14 +3,17 @@ public class Kryptering {
 
         int a = 123;
         int b = 321;
-
+        int c = a^b;
 
         System.out.print(Integer.toBinaryString(a));
         System.out.print(Integer.toBinaryString(b));
-
+        System.out.print(Integer.toBinaryString(c));
         System.out.print(a^b);
+    }
+
+
+    public static void{
 
 
     }
-
 }
