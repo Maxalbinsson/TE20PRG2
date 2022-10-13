4f3cdf69a5aa061c88f7a00f1e96ca826fd60c5a
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-10-13 13:17:03 +0200

    fixat alla fel jag hade.

--- src/model.java
+++ src/model.java
@@ -1,21 +1,18 @@
 public class model {
-
-    public model() {
-    }
-
-    public static void main(String[] args) {
-        String B = "banan";
-        String S = "skor";
-
-        model cryptModel = new model();
-        System.out.println(cryptModel.cryptString(S,B));
-    }
     private String message;
     private String key;
     private String crypt;
 
     public void setMessage(String message){
+    this.message = message;
+    }
 
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getCrypt() {
+        return crypt;
     }
 
     public String cryptString(String message, String Key){
@@ -23,6 +20,8 @@ public class model {
         while (Key.length() < message.length()) {
             Key = expandKey(Key);
         }
+
+
         for (int i = 0 ; 1 <message.length() ; i++) {
             result += (char)crypt(message.charAt(i), Key.charAt(i));
         }
@@ -31,15 +30,29 @@ public class model {
 
 
 
-    private String expandKey(String Key) {return Key+Key;}
+    private String expandKey(String Key) {
+        return Key+Key;}
 
     private int crypt(int S, int B){
         return B^S;
     }
 
-    public static void  (String[] args) {
+    public static void  main(String[] args) {
+        String B = "banan";
+        String S = "skor";
+
         model cryptModel = new model();
+        System.out.println(cryptModel.cryptString(S,B));
+        cryptModel.setMessage(B);
+        cryptModel.setKey(S);
+        cryptModel.cryptString();
+        System.out.println(cryptModel.getCrypt());
     }
 
+    private void cryptString() {
+        while (key.length() < message.length()) {
+            key = expandKey(key);
+                }
+            }
 
 }
